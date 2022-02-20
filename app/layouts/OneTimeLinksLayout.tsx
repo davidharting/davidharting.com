5cb1d57baa1a64e732860da4384f1b333950fb5b
David Harting <david.e.harting@gmail.com>
2022-02-20 12:08:37 -0500

    styling 1-time-links page

---
+++ app/layouts/OneTimeLinksLayout.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+const OneTimeLinksLayout: FC = ({ children }) => {
+  return <div className="m-auto max-w-2xl mt-12 font-sans">{children}</div>;
+};
+
+export default OneTimeLinksLayout;
---
+++ app/routes/one-time-links.tsx
@@ -0,0 +1,42 @@
+import type { FC } from "react";
+import { useTransition, useActionData, Form } from "remix";
+import Layout from "~/layouts/OneTimeLinksLayout";
+
+const OneTimeLinks: FC = () => {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-semibold font-serif">One-Time Links</h1>
+      <p className="mt-8">
+        Create a link to a private message. The contents of your message will be
+        encrypted client-side, so the site owner can't read it. The message will
+        be deleted after 1 hour or when it is viewed, whichever comes first.
+      </p>
+      <hr className="mt-4" />
+      <div className="mt-8">
+        <Form method="post">
+          <fieldset>
+            <div className="flex flex-col">
+              <label htmlFor="message" className="">
+                Secret Message
+              </label>
+              <textarea
+                name="message"
+                rows={3}
+                className="mt-1 p-2 roundeed ring-1 ring-slate-900/10 shadow-sm rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500 caret-teal-500 dark:bg-slate-700 dark:focus:ring-teal-800 dark:caret-teal-800 dark:focus:bg-slate-900"
+              />
+            </div>
+
+            <br />
+            <input
+              type="submit"
+              value="Create"
+              className="bg-teal-700 text-slate-100 px-4 py-2 rounded-md hover:ring-teal-700 hover:ring hover:bg-teal-800 active:ring-0 hover:cursor-pointer"
+            />
+          </fieldset>
+        </Form>
+      </div>
+    </Layout>
+  );
+};
+
+export default OneTimeLinks;
