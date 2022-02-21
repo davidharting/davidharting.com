3e589c7b2ad115d3c674a081b646ed255cc9f017
David Harting <david.e.harting@gmail.com>
2022-02-20 19:27:25 -0500

    lil padding there on mobile

--- app/layouts/OneTimeLinksLayout.tsx
+++ app/layouts/OneTimeLinksLayout.tsx
@@ -1,7 +1,11 @@
 import type { FC } from "react";
 
 const OneTimeLinksLayout: FC = ({ children }) => {
-  return <div className="m-auto max-w-2xl mt-12 font-sans">{children}</div>;
+  return (
+    <div className="m-auto max-w-2xl mt-12 font-sans px-2 md:px-0">
+      {children}
+    </div>
+  );
 };
 
 export default OneTimeLinksLayout;
