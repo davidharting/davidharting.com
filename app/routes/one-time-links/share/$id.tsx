8ee4a7467e923bbdf0048a7225127b356bb01cf8
David Harting <david.e.harting@gmail.com>
2022-02-20 19:42:14 -0500

    share page

--- app/routes/one-time-links/share/$id.tsx
+++ app/routes/one-time-links/share/$id.tsx
@@ -1,12 +1,37 @@
 import type { FC } from "react";
+import { useLoaderData } from "remix";
+import type { LoaderFunction } from "remix";
 import Layout from "~/layouts/OneTimeLinksLayout";
 
 const Complete: FC = () => {
+  const data = useLoaderData<Data>();
   return (
     <Layout>
-      <h1>Share your secret message</h1>
+      <div className="space-y-8">
+        <h1 className="text-3xl font-semibold font-serif">
+          Share your secret message
+        </h1>
+        <p>
+          Copy the link below and share it with your recipient. The link can be
+          used exactly once, and will only work for an hour.
+        </p>
+        <div>
+          <p>{data.link}</p>
+          <button>Copy</button>
+        </div>
+      </div>
     </Layout>
   );
 };
 
 export default Complete;
+
+export const loader: LoaderFunction = ({ request }) => {
+  return {
+    link: request.url.replace("/share", ""),
+  };
+};
+
+interface Data {
+  link: string;
+}
