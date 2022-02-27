ee79e3f229829e58e78fc2e9ee33db6b838a53f1
David Harting <david.e.harting@gmail.com>
2022-02-27 12:03:30 -0500

    whoops

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,6 @@ module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     extend: {
-      // animation: {
-      //   fade: "fade 5s ease-in-out",
-      // },
-      // keyframes: theme => {
-      //   fade: {
-      //     '0%': { hidden: theme('colors.red.300') },
-      //     '100%': { backgroundColor: theme('colors.transparent') },
-      //   }
-      // },
       fontFamily: {
         sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
         serif: ["Source Serif Pro", ...defaultTheme.fontFamily.serif],
