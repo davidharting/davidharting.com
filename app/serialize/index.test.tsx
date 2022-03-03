73b280ed5e5adc81d09686f742b86e456951695d
David Harting <david.e.harting@gmail.com>
2022-03-02 21:53:22 -0500

    Setup jest and demo it's working (#1)

---
+++ app/serialize/index.test.tsx
@@ -0,0 +1,31 @@
+import { serialize } from "cookie";
+import { objectToBase64 } from "~/serialize";
+
+const exampleObjects = [
+  { first: "Tom", last: "Bombadil" },
+  { age: 27 },
+  { a: 1, b: 2, c: 3, d: { e: 4, f: 5 } },
+];
+describe("serialize", () => {
+  describe("#objectToBase64", () => {
+    test.each(exampleObjects)(
+      "The same object should serialize to the same thing each time it is serialized (%o)",
+      (obj) => {
+        const firstResult = objectToBase64(obj);
+        const secondResult = objectToBase64(obj);
+        const serializedCopy = objectToBase64({ ...obj });
+        expect(firstResult).toEqual(secondResult);
+        expect(secondResult).toEqual(serializedCopy);
+      }
+    );
+
+    test.each(exampleObjects)(
+      "serialized objects should not contain = or + (%o)",
+      (obj) => {
+        const serialized = objectToBase64(obj);
+        expect(serialized).not.toContain("+");
+        expect(serialized).not.toContain("=");
+      }
+    );
+  });
+});
