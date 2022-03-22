04325215876ab95679e629f68f7854448d1aae68
David Harting <david.e.harting@gmail.com>
2022-03-21 21:34:49 -0400

    wire up jotai

--- app/routes/picross/index.tsx
+++ app/routes/picross/index.tsx
@@ -1,8 +1,12 @@
 import type { FC } from "react";
 import Heading from "~/element/typography/heading";
 import { TextInput } from "~/form/TextInput";
+import { atom, useAtom } from "jotai";
+
+const patternInputAtom = atom("");
 
 const PicrossPage: FC = () => {
+  const [patternValue, setPatternValue] = useAtom(patternInputAtom);
   return (
     <div className="m-auto max-w-2xl mt-12 font-sans px-2 md:px-0">
       <div className="space-y-4">
@@ -16,8 +20,13 @@ const PicrossPage: FC = () => {
       </div>
       <div className="mt-8">
         <form>
-          {/* <input name="size" type="number" /> */}
-          <TextInput label="Pattern" name="pattern" placeholder="1 3 1" />
+          <TextInput
+            label="Pattern"
+            name="pattern"
+            onChange={(newValue) => setPatternValue(newValue)}
+            placeholder="1 3 1"
+            value={patternValue}
+          />
         </form>
       </div>
     </div>
