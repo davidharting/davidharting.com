750b520b8e9d58d21ad45ef5670f02bc1c3266dc
David Harting <david.harting@hey.com>
2023-03-18 19:51:47 -0400

    factor out that stuff

--- app/Controllers/Http/HomeController.tsx
+++ app/Controllers/Http/HomeController.tsx
@@ -1,22 +1,10 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import type { VNode, FunctionComponent } from 'preact'
 
-import { render as preactRenderToString } from 'preact-render-to-string'
+import { render } from 'App/pages'
+import { HomePage } from 'App/pages/home_page'
 
 export default class HomeController {
   public async show(ctx: HttpContextContract) {
     return render(ctx, <HomePage />)
   }
-}
-
-const HomePage: FunctionComponent = () => {
-  return (
-    <div>
-      <h1>👋 Hey, I am David Harting!</h1>
-    </div>
-  )
-}
-
-const render = (ctx: HttpContextContract, component: VNode) => {
-  return ctx.view.render('layouts/app', { contents: preactRenderToString(component) })
 }
---
+++ app/pages/home_page.tsx
@@ -0,0 +1,9 @@
+import type { FunctionComponent } from 'preact'
+
+export const HomePage: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>👋 Hey, I am David Harting!</h1>
+    </div>
+  )
+}
