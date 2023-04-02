bdfa9f4139ea3e85e85df91f2e4286db5b133643
David Harting <david.harting@hey.com>
2023-04-02 16:36:20 -0400

    use posgres ssl in prod

--- config/database.ts
+++ config/database.ts
@@ -41,6 +41,7 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
+        ssl: Env.get('NODE_ENV') === 'production',
       },
       migrations: {
         naturalSort: true,
