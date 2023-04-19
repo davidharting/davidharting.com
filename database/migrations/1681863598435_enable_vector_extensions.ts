623ac0dc66ba8fdb35a724f70facf61885fdf207
David Harting <david.harting@hey.com>
2023-04-18 20:23:17 -0400

    that was an accident don't tell on me

--- database/migrations/1681863598435_enable_vector_extensions.ts
+++ database/migrations/1681863598435_enable_vector_extensions.ts
@@ -1,8 +1,6 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'enable_vector_extensions'
-
   public async up() {
     this.db.rawQuery('CREATE EXTENSION vector;')
   }
