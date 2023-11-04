637d3ab8d50343d28cafabae3c7e0fc8d0c79344
David Harting <david.harting@hey.com>
2023-11-04 15:37:04 -0400

    dashboard with daisy

---
+++ resources/views/components/type/page-title.blade.php
@@ -0,0 +1,6 @@
+
+<h1 {{$attributes->merge([
+    'class' => 'font-serif text-6xl'
+]) }}>
+    {{ $slot }}
+</h1>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <x-type.page-title class='mb-8'>Dashboard</x-type.page-title>
+
+    <ul>
+    <li><a class='link' href="{{ Route('profile.edit') }}">Profile</a></li>
+    <li>
+        <form method="POST" action="{{ route('logout') }}">	
+            @csrf
+            <input type='submit' value='Log out' class='link' />
+        </form>
+    </li>
+    </ul>
+
+</x-app-layout>
