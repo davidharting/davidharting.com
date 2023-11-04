43bbebd2b6df666d770be3769e19b975e2382141
David Harting <david.harting@hey.com>
2023-11-04 15:44:22 -0400

    password forms with daisy

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,19 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+<x-app-layout>
+    <x-type.page-title>Forgot your password?</x-type.page-title>
+    <div class='space-y-8'>
 
-    <!-- Session Status -->
+    <p>
+        No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+    </p>
+
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <form method="POST" action="{{ route('password.email') }}" class='space-y-8'>
         @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <x-form.input name='email' label='Email' required autofocus />
+        <div class='flex justify-end'>
+            <input type='submit' value='Email me a reset link' class='btn btn-primary' />
         </div>
     </form>
-</x-guest-layout>
+    </div>
+</x-app-layout>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,14 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+<x-app-layout>
+    <x-type.page-title>Reset Password</x-type.page-title>
 
+    <form method="POST" class='mt-8' action="{{ route('password.store') }}">
+        @csrf
         <!-- Password Reset Token -->
         <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
+        <!-- Visible form fields -->
+        <x-form.input name='email' label='Email' required autofocus />
+        <x-form.input name='password' label='Password' type='password' required />
+        <x-form.input name='password_confirmation' label='Confirm Password' type='password' required />
+        <input type='submit' value='Reset password' class='btn btn-primary' />
     </form>
-</x-guest-layout>
+</x-app-layout>
