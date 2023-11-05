9bba3c01820d5db15a6b6da5ced499b8c8cf0830
David Harting <david.harting@hey.com>
2023-11-05 14:24:13 -0500

    Home page content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,29 @@
 <x-layout.app>
-
-    <h1 class='font-serif text-6xl'>
-        Hello!
+    <h1 class='text-5xl font-extrabold font-serif mt-10'>
+        Hi! I am David Harting,
     </h1>
+    <p class='text-lg'>and it's a great day to build software ☀️</p>
+
+
+    <div class='space-y-12 mt-8'>
+        <div class='space-y-6'>
+            <h2 class='font-serif font-semibold text-3xl'>About me</h2>
+            <p>I am an experienced, full-stack software engineer from Westfield, Indiana. My focus in my career has been web apps that enable people to work with data. I am now working as an engineering manager at dbt Labs, building a web-based IDE for analytics engineers.</p>
+            <p>At work, I am happiest working closely with product and design to navigate tradeoffs and to ship quickly. I am passionate about code review and testing.</p>
+            <p>I believe in working hard and living slow. I enjoy life with my wife and my dog. I am fortunate enough to enjoy leisure time, which is filled with walks, wine, books, and games.</p>
+        </div>
+        <div class='space-y-6'>
+            <h2 class='font-serif font-semibold text-3xl'>Let's connect</h2>
+            <p>You can email me at
+                <span class='link link-primary'>connect@davidharting.com</span>.
+                You can also find me on
+                    <a href='https://github.com/davidharting' class='link link-primary'>GitHub</a>,
+                    <a href='https://twitter.com/davehrtng' class='link link-primary'>Twitter</a>,
+                    and
+                    <a href='https://www.linkedin.com/in/davidharting/' class='link link-primary'>LinkedIn</a>.
+            </p>
+        </div>
+    </div>
+
 
 </x-layout.app>
