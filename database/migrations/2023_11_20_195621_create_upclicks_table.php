3aa65ab42bb2cffc4db525221bc8cdf315f64638
David Harting <david.harting@hey.com>
2023-11-21 14:13:21 -0500

    Add userID to upclicks

--- database/migrations/2023_11_20_195621_create_upclicks_table.php
+++ database/migrations/2023_11_20_195621_create_upclicks_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('upclicks', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('user_id')->nullable()->constrained();
+
+            $table->index(['user_id']);
         });
     }
 
