7a0a48f7bfd267dc239963160af2bd82d143ca2e
David Harting <david.harting@hey.com>
2023-11-25 15:46:15 -0500

    Make card header optional

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,8 +1,10 @@
 <section class="card bg-slate-800 text-neutral-content">
     <div class="card-body">
-        <header class='card-title'>
-            {{ $title }}
-        </header>
+        @isset($title)
+            <header class='card-title'>
+                {{ $title }}
+            </header>
+        @endisset
         {{ $slot }}
     </div>
 </section>
