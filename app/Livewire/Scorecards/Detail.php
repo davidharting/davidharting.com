46aaab57165fed57910098e8f9925e431e4b04cb
David Harting <david.harting@hey.com>
2023-11-30 16:01:36 -0500

    Render scores

--- app/Livewire/Scorecards/Detail.php
+++ app/Livewire/Scorecards/Detail.php
@@ -2,7 +2,11 @@
 
 namespace App\Livewire\Scorecards;
 
+use App\Models\Score;
 use App\Models\Scorecard;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Computed;
 use Livewire\Attributes\Locked;
 use Livewire\Component;
@@ -26,6 +30,30 @@ class Detail extends Component
     #[Computed]
     public function playerNames(): array
     {
-        return $this->scorecard->players->pluck('name')->toArray();
+        return $this->scorecard->players()->orderBy('id', 'asc')->pluck('name')->toArray();
+    }
+
+    /**
+     * Returns a 2D array. Each item represents a round. Each
+     */
+    #[Computed]
+    public function rounds(): Collection
+    {
+        $collection =  Score::whereIn('player_id', function (Builder $query) {
+            $query->select('player_id')->from('scorecards')->where('id', $this->scorecard->id);
+        })
+            ->groupBy('round')
+            ->orderBy('round', 'asc')
+            ->orderBy('player_id', 'asc')
+            ->select('round as round_number')->addSelect(DB::raw('json_group_array(score) as round_scores'))
+            ->get();
+
+
+
+        $data = $collection->map(function ($item) {
+            return array_merge([$item->round_number], json_decode($item->round_scores));
+        });
+
+        return $data;
     }
 }
--- resources/views/livewire/scorecards/detail.blade.php
+++ resources/views/livewire/scorecards/detail.blade.php
@@ -2,11 +2,20 @@
     <table class='table'>
         <thead>
             <tr>
-                <th></th> {{-- Empty cell for round number --}}
+                <th>Round</th>
                 @foreach ($this->playerNames as $name)
                     <th>{{ $name }}</th>
                 @endforeach
             </tr>
         </thead>
+        @foreach ($this->rounds as $round)
+            <tr>
+                @foreach ($round as $cell)
+                    <td>
+                        {{ $cell }}
+                    </td>
+                @endforeach
+            </tr>
+        @endforeach
     </table>
 </div>
