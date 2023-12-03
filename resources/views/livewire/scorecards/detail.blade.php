a8512157667633a1290a01466c76837a2d79faa3
David Harting <david.harting@hey.com>
2023-12-03 16:57:30 -0500

    Create drawer for new record entry

--- resources/views/livewire/scorecards/detail.blade.php
+++ resources/views/livewire/scorecards/detail.blade.php
@@ -1,41 +1,62 @@
-<x-card>
-    <div class='overflow-x-auto'>
-        <table class='table table-pin-rows table-pin-cols'>
-            <thead>
-                <tr>
-                    <th>Round</th>
-                    @foreach ($this->playerNames as $name)
-                        <th>{{ $name }}</th>
-                    @endforeach
-                </tr>
-            </thead>
+<div class='drawer drawer-end'>
+    <input id="the-drawer" type="checkbox" class="drawer-toggle" />
+    <div class='drawer-content'>
+        <x-card>
+            <label for="the-drawer" class="btn btn-primary drawer-button">Record new round</label>
+            <div class='overflow-x-auto'>
+                <table class='table'>
+                    <thead>
+                        <tr>
+                            <th>Round</th>
+                            @foreach ($this->playerNames as $name)
+                                <th>{{ $name }}</th>
+                            @endforeach
+                        </tr>
+                    </thead>
 
-            @foreach ($this->rounds as $round)
-                <tr>
-                    @foreach ($round as $cell)
-                        @if ($loop->first)
-                            <th>
-                                {{ $cell }}
-                            </th>
-                            @continue
-                        @else
-                            <td>
-                                {{ $cell }}
-                            </td>
-                        @endif
+                    @foreach ($this->rounds as $round)
+                        <tr>
+                            @foreach ($round as $cell)
+                                @if ($loop->first)
+                                    <th>
+                                        {{ $cell }}
+                                    </th>
+                                    @continue
+                                @else
+                                    <td>
+                                        {{ $cell }}
+                                    </td>
+                                @endif
+                            @endforeach
+                        </tr>
                     @endforeach
-                </tr>
-            @endforeach
 
-            <tfoot>
-                <tr>
-                    @foreach ($this->totals as $total)
-                        <td>
-                            {{ $total }}
-                        </td>
-                    @endforeach
-                </tr>
-            </tfoot>
-        </table>
+                    <tfoot>
+                        <tr>
+                            @foreach ($this->totals as $total)
+                                <td>
+                                    {{ $total }}
+                                </td>
+                            @endforeach
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </x-card>
     </div>
-</x-card>
+
+    <div class='drawer-side'>
+        <label for="the-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <div class='min-h-full bg-base-200 p-4 text-base-content opacity-100'>
+            <h2 class='font-serif mt-4 text-3xl'>Record new round</h2>
+            <form class='mt-4 space-y-4'>
+                @foreach ($this->playerNames as $name)
+                    <x-form.input name='player.{{ $loop->index }}' :label='$name' />
+                @endforeach
+                <div class='flex justify-end'>
+                    <button type='submit' class='btn btn-primary w-3/4 mt-4'>Add</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
