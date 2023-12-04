c7e21513299137c8c29b37f54d6d00ce2398ed0b
David Harting <david.harting@hey.com>
2023-12-04 13:05:28 -0500

    display created_at

--- resources/views/scorecards/show.blade.php
+++ resources/views/scorecards/show.blade.php
@@ -1,5 +1,6 @@
 <x-layout.app>
     <x-type.page-title>{{ $scorecard->title }}</x-type.page-title>
+    <p>Started on {{ $scorecard->created_at->format('Y F j') }}</p>
 
     <div class="mt-12">
         <livewire:scorecards.detail :scorecard="$scorecard" />
