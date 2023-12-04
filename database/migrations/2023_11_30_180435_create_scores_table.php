24581a0d70fccd938d584854a636bfff48d538a7
David Harting <david.harting@hey.com>
2023-12-04 13:04:55 -0500

    timezone timestamps >>>>

--- database/migrations/2023_11_30_180435_create_scores_table.php
+++ database/migrations/2023_11_30_180435_create_scores_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('scores', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->timestampsTz();
 
             $table->integer('round');
             $table->integer('score');
