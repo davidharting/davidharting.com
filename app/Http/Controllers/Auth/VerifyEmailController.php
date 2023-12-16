d2b08b3333f6c8d7e5c3da83c84cae5ee8747f2a
David Harting <david.harting@hey.com>
2023-12-16 15:29:11 -0500

    no error to ignore

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -20,7 +20,7 @@ class VerifyEmailController extends Controller
         }
 
         if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user())); // @phpstan-ignore-line
+            event(new Verified($request->user()));
         }
 
         return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
