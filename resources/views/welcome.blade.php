d43a2800e84e0a244724a9f9097b4ab49864d930
David Harting <david.harting@hey.com>
2023-12-21 13:02:50 -0500

    link to scorecards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,8 +63,9 @@
         </div>
     </div>
 
-    <div>
+    <div class='flex justify-start space-x-8'>
         <a href='{{ route('intervals.show') }}' wire:navigate class='link link-primary'>Interval Timer</a>
+        <a href='{{ route('scorecards.create') }}' wire:navigate class='link link-primary'>Scorecards</a>
     </div>
 
 </x-layout.app>
