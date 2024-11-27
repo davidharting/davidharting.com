7fe3d7fb49f59f8c534b9a958a54c6f9a4786e6f
David Harting <hartingdavid@icloud.com>
2024-11-26 22:17:38 -0500

    Add db notifications and job batches
    These are requirements for CSV importer in filament

---
+++ database/migrations/2024_11_27_030702_create_notifications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->jsonb('data');
+            $table->timestampTz('read_at')->nullable();
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
