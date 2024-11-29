8f48849139c8285f4c9a9bcf4a103902816584d2
David Harting <hartingdavid@icloud.com>
2024-11-29 00:04:01 -0500

    filament resource

---
+++ app/Filament/Resources/NoteResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\NoteResource\Pages;
+use App\Models\Note;
+use Carbon\Carbon;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class NoteResource extends Resource
+{
+    protected static ?string $model = Note::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('slug'),
+                Forms\Components\DatePicker::make('published_at')->default(Carbon::now())->label('Publish Date'),
+                Forms\Components\TextInput::make('title')
+                    ->columnSpanFull(),
+                Forms\Components\Textarea::make('lead')
+                    ->columnSpanFull(),
+                Forms\Components\RichEditor::make('content')->columnSpanFull(),
+                Forms\Components\Toggle::make('visible')->default(true)
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\TextColumn::make('title')->lineClamp(50),
+                Tables\Columns\TextColumn::make('lead')->lineClamp(50),
+                Tables\Columns\IconColumn::make('visible')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNotes::route('/'),
+            'create' => Pages\CreateNote::route('/create'),
+            'edit' => Pages\EditNote::route('/{record}/edit'),
+        ];
+    }
+}
