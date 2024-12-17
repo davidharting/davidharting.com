cf5d1bdf9f037130a7e8db6685c1f57010763f25
David Harting <hartingdavid@icloud.com>
2024-12-17 16:57:39 -0500

    backlog query

---
+++ app/Queries/Media/BacklogQuery.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Queries\Media;
+
+use Illuminate\Support\Facades\DB;
+
+class BacklogQuery
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function execute()
+    {
+        return DB::table('media')
+            ->join('media_types', 'media.media_type_id', '=', 'media_types.id')
+            ->leftJoin('creators', 'media.creator_id', '=', 'creators.id')
+            ->leftJoin('media_events', 'media_events.media_id', '=', 'media.id')
+
+            ->select(
+                'media.title as title',
+                'creators.name as creator',
+                'media_types.name as type',
+                'media.created_at as added_at',
+            )
+
+            ->whereNull('media_events.id')
+
+            ->orderBy('media.created_at', 'desc')
+            ->get();
+    }
+}
---
+++ tests/Feature/Queries/Media/BacklogQueryTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Creator;
+use App\Models\Media;
+use App\Models\MediaEvent;
+use App\Queries\Media\BacklogQuery;
+use Carbon\Carbon;
+use Tests\TestCase;
+
+test('backlog query test', function () {
+    /** @var TestCase $this */
+    // Create distractor media with different events
+    Media::factory()
+        ->movie()
+        ->has(MediaEvent::factory()->finished()->state(['occurred_at' => '2023-01-01']), 'events')
+        ->create(['title' => 'Finished Media']);
+
+    Media::factory()
+        ->book()
+        ->for(Creator::factory(['name' => 'Author 2']))
+        ->has(MediaEvent::factory()->started()->state(['occurred_at' => '2023-01-02']), 'events')
+        ->create(['title' => 'Started Media']);
+
+    Media::factory()
+        ->book()
+        ->for(Creator::factory(['name' => 'Author 3']))
+        ->has(MediaEvent::factory()->abandoned()->state(['occurred_at' => '2023-01-03']), 'events')
+        ->create(['title' => 'Abandoned Media']);
+
+    // Create additional media with no events
+    Media::factory(['created_at' => Carbon::parse('2023-01-08')])
+        ->book()
+        ->for(Creator::factory(['name' => 'Author 4']))
+        ->create(['title' => 'Backlog Book']);
+
+    Media::factory(['created_at' => Carbon::parse('2023-01-05')])
+        ->movie()
+        ->create(['title' => 'Backlog Movie']);
+
+    $result = (new BacklogQuery)->execute();
+
+    expect($result)->toHaveCount(2);
+    expect($result->first()->title)->toBe('Backlog Book');
+    expect($result->first()->creator)->toBe('Author 4');
+    expect($result->last()->title)->toBe('Backlog Movie');
+});
