f29a7bed9554d0416b144a871c55337e01e9097f
David Harting <hartingdavid@icloud.com>
2025-01-22 10:24:36 -0500

    test db backup

--- tests/Feature/Jobs/BackupDatabaseTest.php
+++ tests/Feature/Jobs/BackupDatabaseTest.php
@@ -1,7 +1,42 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Jobs\BackupDatabase;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
 
-    $response->assertStatus(200);
+test('happy path', function () {
+    /** @var TestCase $this */
+
+    Storage::fake('local');
+
+    Process::fake([
+        'pg_dump *' => Process::result(output: 'some tar file'),
+        'gzip' => Process::result(output: 'compressed tar file')
+    ]);
+
+    BackupDatabase::dispatchSync('backup');
+
+
+    Storage::assertExists('backup', "compressed tar file\n");
+});
+
+test('failure', function () {
+    /** @var TestCase $this */
+    Storage::fake('local');
+
+    Process::fake([
+        'pg_dump *' => Process::result(exitCode: 1, errorOutput: 'pg_dump: error'),
+        'gzip' => Process::result(output: 'compressed tar file')
+    ]);
+
+
+    $this->assertThrows(function () {
+        BackupDatabase::dispatchSync('backup');
+    }, RuntimeException::class);
+
+
+    Process::assertDidntRun('gzip');
+
+    $this->assertFalse(Storage::exists('backup'));
 });
