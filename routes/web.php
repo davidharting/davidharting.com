5249a81b6f62dffd253d91d0983f15c16045159f
David Harting <hartingdavid@icloud.com>
2025-03-22 12:18:31 -0400

    Add feed rroutes

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::feeds();
+
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
@@ -53,4 +55,4 @@ if (env('APP_ENV') == 'local') {
     });
 }
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
