b257174fced2e1f0f00de503bc150794f0673f80
David Harting <hartingdavid@icloud.com>
2025-04-14 21:01:07 -0400

    test: only admins can see edit media page
    Sanity check the filament gating

---
+++ tests/Feature/Http/Filament/Media/EditMediaTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Media;
+use App\Models\User;
+use Tests\TestCase;
+use League\HTMLToMarkdown\HtmlConverter;
+
+
+test('anonymous user not allowed', function () {
+    /** @var TestCase $this */
+    $media = Media::factory()->create();
+
+    $response = $this->get(route('filament.admin.resources.media.edit', $media));
+
+    $response->assertRedirect(route('filament.admin.auth.login'));
+});
+
+test('regular user not allowed', function () {
+    /** @var TestCase $this */
+    $user = User::factory()->create();
+    $media = Media::factory()->create();
+
+    $response = $this->actingAs($user)->get(route('filament.admin.resources.media.edit', $media));
+
+    $response->assertStatus(403);
+});
+
+test('Visible to admin', function () {
+    /** @var TestCase $this */
+    $media = Media::factory()->create(['title' => 'The Hobbit']);
+    $user = User::factory()->admin()->create();
+
+    $response = $this->actingAs($user)->get(route('filament.admin.resources.media.edit', $media));
+
+    $response->assertStatus(200);
+    $response->assertSeeText('Edit Media');
+});
