a6c419ee0783b555afbf436d2af650d344b645bf
David Harting <hartingdavid@icloud.com>
2025-06-10 22:32:49 -0400

    fluxify welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,19 @@
 <x-layout.app>
     <div class="m-auto mx-4 sm:mx-auto max-w-2xl my-16">
         <div
-            class="flex flex-col sm:flex-row justify-between w-full sm:space-x-4"
+            class="flex flex-col sm:flex-row w-full sm:space-x-4"
         >
-            <div class="avatar mb-4 sm:mb-0 flex justify-center">
-                <div
-                    class="w-24 rounded-xl ring-3 ring-primary ring-offset-base-100 ring-offset-2"
-                >
-                    <img src="headshot.jpg" />
-                </div>
-            </div>
-            <div class="space-y-2">
-                <h1 class="text-5xl font-extrabold font-serif">
+            <flux:avatar src="headshot.jpg" size='xl' />
+            <div>
+                <flux:heading class="font-serif" size='xl' level='1'>
                     Hi! I am David Harting,
-                </h1>
-                <p class="text-lg">and it's a great day to build software ☀️</p>
+                </flux:heading>
+                <flux:text class="">and it's a great day to build software ☀️</flux:text>
             </div>
         </div>
 
-        <div class="space-y-16 mt-14">
-            <div class="space-y-6">
-                <div class="space-y-12">
+        <div class="mt-9">
+            <div class="prose prose-sm prose-pink dark:prose-invert">
                     <p>
                         I am an experienced, full-stack software engineer from
                         Westfield, Indiana. My focus in my career has been web
