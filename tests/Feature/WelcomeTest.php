67d971224ac0da22e8f74ad1e604c9a65c855696
David Harting <hartingdavid@icloud.com>
2025-08-12 17:24:12 -0400

    Add stale-while-revalidate headers to home page
    The goal is to serve index.html from the edge and then in the background update the cloud flare cache
    
    The test is failing right now. This is because there is a livewire component on the home page which is overriding caching behavior. I think it has something to do with a default back-button browser history setting

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -10,3 +10,16 @@ test('Welcome page contains link to atom feed', function () {
     $feedLink = '<link rel="alternate" type="application/atom+xml" href="http://davidharting-dot-com.test/feed" title="David Harting">';
     $response->assertSeeHtml($feedLink);
 });
+
+test('Welcome page is public and has swr headers', function () {
+    /** @var TestCase $this */
+    $response = $this->get('/');
+    $response->assertStatus(200);
+
+    $response->assertHeader(
+        'Cache-Control',
+        'max-age=60, public, stale-while-revalidate=3600'
+    );
+
+    $response->assertHeader('etag');
+});
