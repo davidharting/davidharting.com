3cd3344694c15f3c03d55a7b5640e3f70afd4050
David Harting <hartingdavid@icloud.com>
2025-09-12 22:52:38 -0400

    tests

---
+++ tests/Feature/Http/Fileshare/FileshareShowTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+test('requires auth', function () {
+    /** @var TestCase $this */
+    $response = $this->get('/fileshare/the-path.txt');
+    $response->assertRedirect('/login');
+});
+
+test('happy path', function () {
+    /** @var TestCase $this */
+    Storage::fake();
+
+    Storage::put('the-path.txt', 'contents');
+
+    $response = $this->actingAs(User::factory()->createOne())->get('/fileshare/the-path.txt');
+    $response->assertStatus(200);
+    $response->assertSeeInOrder(['Size', '8', 'Visibility']);
+});
