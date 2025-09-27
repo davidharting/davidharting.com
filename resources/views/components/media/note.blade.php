159400ad2ddda5065277539284f824af32ec4534
David Harting <hartingdavid@icloud.com>
2025-09-27 19:28:41 -0400

    Deal with newlines

--- resources/views/components/media/note.blade.php
+++ resources/views/components/media/note.blade.php
@@ -1,7 +1,7 @@
 @can("seeNote", App\Models\Media::class)
     @if ($item->note)
-        <div class="text-xs text-gray-600">
-            {{ $item->note }}
+        <div class="text-xs text-gray-600 whitespace-pre-line">
+            {{ trim($item->note) }}
         </div>
     @endif
 @endcan
