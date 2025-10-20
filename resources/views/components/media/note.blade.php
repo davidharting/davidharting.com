69a69fa577547aad51e302fb3bc82a3a1554fc3f
David Harting <hartingdavid@icloud.com>
2025-10-20 19:23:31 -0400

    show finished comment via new column

--- resources/views/components/media/note.blade.php
+++ resources/views/components/media/note.blade.php
@@ -1,7 +1,13 @@
 @can("seeNote", App\Models\Media::class)
     @if ($item->note)
-        <div class="text-xs text-gray-600 whitespace-pre-line">
+        <div class="text-xs text-gray-600">
             {{ trim($item->note) }}
+        </div>
+    @endif
+
+    @if ($item->finished_comment)
+        <div class="text-xs text-gray-600">
+            {{ trim($item->finished_comment) }}
         </div>
     @endif
 @endcan
