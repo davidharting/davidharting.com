ef41e542835f83a1e7147a0c75f8c71fb42aff1d
David Harting <hartingdavid@icloud.com>
2025-12-02 13:23:34 -0500

    relocate test

--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -10,3 +10,11 @@ test('Welcome page contains link to atom feed', function () {
     $feedLink = '<link rel="alternate" type="application/atom+xml" href="http://davidharting-dot-com.test/feed" title="David Harting">';
     $response->assertSeeHtml($feedLink);
 });
+
+test('pages link appears in navigation', function () {
+    /** @var TestCase $this */
+    $response = $this->get('/');
+    $response->assertSuccessful();
+    $response->assertSee('Pages');
+    $response->assertSee(route('pages.index'));
+});
