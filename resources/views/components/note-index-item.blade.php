47280e8ed2918ffcb8b86466cb66de3556d2c2d7
David Harting <hartingdavid@icloud.com>
2026-02-04 19:41:20 -0500

    format

--- resources/views/components/note-index-item.blade.php
+++ resources/views/components/note-index-item.blade.php
@@ -1,13 +1,10 @@
 <div class="flex gap-4">
     <div class="w-16 shrink-0 text-sm text-base-content/60">
-        {{ $note->published_at->format('M j') }}
+        {{ $note->published_at->format("M j") }}
     </div>
     <article>
         <p>
-            <a
-                class="link-hover"
-                href="{{ route("notes.show", $note) }}"
-            >
+            <a class="link-hover" href="{{ route("notes.show", $note) }}">
                 @if ($note->title)
                     {{ $note->title }}
                 @else
