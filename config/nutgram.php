465ddf339d24e5cb4836752767c160a0f231d49f
David Harting <hartingdavid@icloud.com>
2026-02-14 20:27:08 -0500

    only david middlware

--- config/nutgram.php
+++ config/nutgram.php
@@ -4,6 +4,8 @@ return [
     // The Telegram BOT api token
     'token' => env('TELEGRAM_TOKEN'),
 
+    'david' => env('TELEGRAM_DAVID_ID'),
+
     // if the webhook mode must validate the incoming IP range is from a telegram server
     'safe_mode' => env('APP_ENV', 'local') === 'production',
 
