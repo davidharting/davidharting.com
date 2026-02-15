43b9be6e92d1f97d7d1fe69fb7b505d8470a6e79
David Harting <hartingdavid@icloud.com>
2026-02-14 19:52:16 -0500

    install nutgram with basic "route"s

---
+++ routes/telegram.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var SergiX44\Nutgram\Nutgram $bot */
+
+use SergiX44\Nutgram\Nutgram;
+
+/*
+|--------------------------------------------------------------------------
+| Nutgram Handlers
+|--------------------------------------------------------------------------
+|
+| Here is where you can register telegram handlers for Nutgram. These
+| handlers are loaded by the NutgramServiceProvider. Enjoy!
+|
+*/
+
+$bot->onCommand('example', function (Nutgram $bot) {
+    $bot->sendMessage('Hello, world!');
+})->description('An example command');
+
+$bot->onMessage(function (Nutgram $bot) {
+    $bot->sendMessage('I cannot respond to general conversation yet');
+});
