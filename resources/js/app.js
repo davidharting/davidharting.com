383bca723ad4f17f39d9fcbf2c13eb3b69655ed4
David Harting <hartingdavid@icloud.com>
2026-03-02 22:08:41 -0500

    format

--- resources/js/app.js
+++ resources/js/app.js
@@ -25,5 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
         .register("/sw.js")
-        .catch((err) => console.error("Service worker registration failed:", err));
+        .catch((err) =>
+            console.error("Service worker registration failed:", err),
+        );
 }
