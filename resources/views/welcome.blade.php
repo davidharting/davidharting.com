be7bd7a504ef569fe454d928a6d92b31c3a968ff
David Harting <hartingdavid@icloud.com>
2026-06-29 09:24:14 -0400

    tweak

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,7 @@
                 <h1 class="text-5xl font-extrabold font-serif">
                     Hi! I am David Harting,
                 </h1>
+                <p> this is a pr preview env</p>
                 <p class="text-lg">and it's a great day to build software ☀️</p>
             </div>
         </div>
